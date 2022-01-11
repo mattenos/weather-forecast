d341564c64837116bf235e4574c8d9d223cff13d
unknown <mattenos4@gmail.com>
2022-01-11 18:27:09 -0500

    targeted the DOM with multiple variables in js file.

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,14 @@ var state;
 var country;
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
+var userInput = document.getElementById('search-input');
+var searchBtn = document.getElementById('search-button');
+var searchList = document.getElementById('search-list');
+var currentEl = document.getElementById('current');
+var forecastEl = document.getElementById('forecast');
+
+
+
 fetch(queryURL)
 
 api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}
